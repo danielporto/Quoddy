5b5775801b4a9a066afdafb0c1dc8d3b4f98081f
Daniel Porto <daniel.porto@gmail.com>
2012-04-28 10:15:35 -0300

    generate images now generate links too

--- profilepics/generate_images.sh
+++ profilepics/generate_images.sh
@@ -10,22 +10,51 @@ TOTAL_IMAGES=`wc -l $THUMBNAILS_DIR/pictures.txt | cut -f 1 -d " "`
 
 IMAGE_ROOT=/var/tmp/Quoddy/profilepics
 
-if [ $1 ];
-then 
+
+#//FUNCTIONS
+
+help(){
+	echo "this is the help message"
+}
+
+generate_images(){
 	NUMBER_OF_USERS=$1
 	echo "Generating $NUMBER_OF_USERS images for users profile as you requested"
 	for (( i=0; i < ${NUMBER_OF_USERS}; i++ )) do 
 		a=`shuf -i 1-${TOTAL_IMAGES} -n 1`;  
 		p=`sed -n ${a}p $THUMBNAILS_DIR/pictures.txt`; 
-		mkdir -p "${IMAGE_ROOT}/${USER_PREFIX}${i}";
-		cp ${p} ${IMAGE_ROOT}/${USER_PREFIX}${i}/$USER_PREFIX${i}_profile_thumbnail48x48.jpg; 
+		#mkdir -p "${IMAGE_ROOT}/${USER_PREFIX}${i}";
+		#cp ${p} ${IMAGE_ROOT}/${USER_PREFIX}${i}/$USER_PREFIX${i}_profile_thumbnail48x48.jpg;
+		cp ${p} ${IMAGE_ROOT}/$USER_PREFIX${i}_profile_thumbnail48x48.jpg; 
 	done
-else
-	echo "Generating $NUMBER_OF_USERS images for users profile"
-	for (( i=0;i<20;i++ )) do 
+
+}
+
+generate_links(){
+	NUMBER_OF_USERS=$1
+	echo "Generating $NUMBER_OF_USERS images for users profile as you requested"
+	for (( i=0; i < ${NUMBER_OF_USERS}; i++ )) do 
 		a=`shuf -i 1-${TOTAL_IMAGES} -n 1`;  
 		p=`sed -n ${a}p $THUMBNAILS_DIR/pictures.txt`; 
-		mkdir -p "${IMAGE_ROOT}/testuser${i}";
-		cp ${p} ${IMAGE_ROOT}/testuser${i}/testuser${i}_profile_thumbnail48x48.jpg; 
+		#mkdir -p "${IMAGE_ROOT}/${USER_PREFIX}${i}";
+		#ln -s `pwd`/${p} ${IMAGE_ROOT}/${USER_PREFIX}${i}/$USER_PREFIX${i}_profile_thumbnail48x48.jpg; 
+		ln -s `pwd`/${p} ${IMAGE_ROOT}/$USER_PREFIX${i}_profile_thumbnail48x48.jpg;
 	done
+}
+
+
+#//MAIN
+
+if [ -z $2 ]; 
+then 
+		echo "You must tell the number of images to create"
+		exit 0;
 fi
+mkdir -p "${IMAGE_ROOT}";
+case $1 in 
+	"links") 			generate_links $2;;						
+	"images") 			generate_images $2;;
+	*) 						help;;
+esac
+echo "Script finished"
+
